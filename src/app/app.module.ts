a131874010ab79c7187374f3ef8ed8b45e84142d
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2022-02-02 17:09:56 +0000

    Alternative GraphQL query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client';
+import { GraphQLModule } from './graphql.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,21 +22,9 @@ import { ExchangeRatesComponent } from './exchange-rates-component';
     HttpClientModule,
     AppRoutingModule,
     ChartModule,
+    GraphQLModule,
   ],
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: 'https://48p1r2roz4.sse.codesandbox.io',
-          }),
-        };
-      },
-      deps: [HttpLink],
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
