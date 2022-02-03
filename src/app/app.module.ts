b78d691429dadee1e965ec474e7e4710ba89649e
Prabhat Thapa <prabhat4ever@gmail.com>
2022-02-03 15:59:55 +0530

    Integrate GraphQL with Apollo Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header.component';
 import { FooterComponent } from './shared/footer.component';
+import { ExchangeRates } from './exchange-rates.component';
 import { ChartModule } from 'primeng/chart';
+
+import {HttpClientModule} from '@angular/common/http';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+
+const uri = 'https://48p1r2roz4.sse.codesandbox.io'; // our GraphQL API
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ExchangeRates,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ChartModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: (httpLink: HttpLink) => {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        }),
+      };
+    },
+    deps: [HttpLink],
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
